4aa9d62375888fb5380fdd5e92fd183a80694293
sunling <sunling621@gmail.com>
2026-09-16 09:21:01 -0700

    refactor: enable unified record edits in standalone stores

--- src/storage/create-records-store.ts
+++ src/storage/create-records-store.ts
@@ -1,7 +1,7 @@
 import type { AppConfig } from "../config.js";
 import { GitHubRecordsStore } from "./github-records.js";
 import { LocalRecordsStore } from "./local-records.js";
-import { withNoteEditing } from "./note-editing.js";
+import { withRecordEditing } from "./record-editing.js";
 import type { RecordsStore } from "./records-store.js";
 
 export function createRecordsStore(config: AppConfig): RecordsStore {
@@ -12,9 +12,9 @@ export function createRecordsStore(config: AppConfig): RecordsStore {
       token: config.githubToken!,
       branch: config.githubBranch,
     };
-    return withNoteEditing(new GitHubRecordsStore(options), options);
+    return withRecordEditing(new GitHubRecordsStore(options), options);
   }
-  return withNoteEditing(new LocalRecordsStore(config.recordsRepoPath), {
+  return withRecordEditing(new LocalRecordsStore(config.recordsRepoPath), {
     kind: "local", root: config.recordsRepoPath,
   });
 }
